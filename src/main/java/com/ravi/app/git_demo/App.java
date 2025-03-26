729486c8d80d7be47da02fe69410597c8212fd22
ravis <ravisva@gmail.com>
2025-03-26 11:39:13 +0530

    added git_demo project to local repo

---
+++ src/main/java/com/ravi/app/git_demo/App.java
@@ -0,0 +1,10 @@
+package com.ravi.app.git_demo;
+
+public class App {
+    public void addDate(String data){
+        System.out.println("data = " + data);
+    }
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+}
