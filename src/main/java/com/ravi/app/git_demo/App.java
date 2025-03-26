c39326e283633e27493788f2597c46564e27d606
ravis <ravisva@gmail.com>
2025-03-26 11:44:25 +0530

    modified App.java

--- src/main/java/com/ravi/app/git_demo/App.java
+++ src/main/java/com/ravi/app/git_demo/App.java
@@ -3,6 +3,9 @@ package com.ravi.app.git_demo;
 public class App {
     public void addDate(String data){
         System.out.println("data = " + data);
+        if (data.equals("Ravi")){
+            System.out.println("Welcome");
+        }
     }
     public static void main(String args[]){
         System.out.println("Hello World");
